c7a94a7f37254a1e927ec9c5d9309c42bc995590
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-06 05:35:36 -0600

    update footer classes

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,19 +1,19 @@
 export default function Footer() {
   return (
     <>
-      <nav class="navbar fixed-bottom bg-body-tertiary">
-        <div class="container-fluid d-flex-inline justify-content-evenly">
-          <a class="nav-item" href="https://github.com/Denitrix">
+      <nav className="navbar fixed-bottom bg-body-tertiary">
+        <div className="container-fluid d-flex-inline justify-content-evenly">
+          <a className="nav-link" href="https://github.com/Denitrix">
             GitHub
           </a>
           <a
-            class="nav-item"
+            className="nav-link"
             href="https://www.linkedin.com/in/alexander-majorowicz-7a9386298/"
           >
             LinkedIn
           </a>
           <a
-            class="nav-item"
+            className="nav-link"
             href="https://stackoverflow.com/users/23354387/alex-majorowicz"
           >
             Stack Overflow
